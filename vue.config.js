7b3a23649181e41aa039625b5b8ba4fd76a1a7bc
guoxudong04 <guoxudong04@baidu.com>
2023-04-26 16:39:41 +0800

    eslint 编译报错处理

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  lintOnSave: false,
   css: {
     loaderOptions: {
       stylus: {
